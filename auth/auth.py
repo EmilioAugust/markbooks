341f992a64b403432a54db0aa42a4ffba8d21f96
EmilioAugust <kuluev.emil.8@gmail.com>
2026-05-06 10:42:08 +0500

    Updated auth/auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -1,42 +1,45 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from database.database import Session, Users
+from sqlalchemy.orm import Session
+from database.database import Users, get_db
 from models.models import CreateUser
 from services.services import get_password_hash, verify_password, create_access_token, decode_token
 from datetime import timedelta
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
-session = Session()
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
-@auth_router.post('/register')
-async def register_user(user: CreateUser):
-    existing_user = session.query(Users).filter(Users.username == user.username).first()
-    if existing_user:
-        raise HTTPException(status_code=400, detail="User already exists.")
-    hashed_password = await get_password_hash(user.password)
-    new_user = Users(username=user.username, email=user.email, hashed_password=hashed_password)
-
-    session.add(new_user)
-    session.commit()
-    session.refresh(new_user)
-    return {'message': 'User registered!'}
-
-@auth_router.post('/token')
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = session.query(Users).filter(Users.username == form_data.username).first()
-    if not user or not await verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail='Wrong username or password')
-    access_token = await create_access_token(data={'id': user.id}, expires_delta=timedelta(days=10))
-    return {'access_token': access_token, 'token_type': 'bearer'}
-
-@auth_router.get('/protected/')
-async def protected_route(token: str = Depends(oauth2_bearer)):
+async def get_current_user(token: str = Depends(oauth2_bearer), db: Session = Depends(get_db)):
     payload = await decode_token(token)
     user_id = payload.get('id')
-    existing_user = session.query(Users).filter(Users.id == user_id).first()
+    existing_user = db.query(Users).filter(Users.id == user_id).first()
     if payload is None:
         raise HTTPException(status_code=401, detail='Wrong token')
     if not existing_user:
         raise HTTPException(status_code=401, detail='User not found')
     return existing_user
+
+@auth_router.post('/register')
+async def register_user(user: CreateUser, db: Session = Depends(get_db)):
+    existing_user = db.query(Users).filter(Users.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists.")
+    hashed_password = await get_password_hash(user.password)
+    new_user = Users(username=user.username, email=user.email, hashed_password=hashed_password)
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {'message': 'User registered!'}
+
+@auth_router.post('/token')
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(Users).filter(Users.username == form_data.username).first()
+    if not user or not await verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail='Wrong username or password')
+    access_token = await create_access_token(data={'id': user.id}, expires_delta=timedelta(days=10))
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+@auth_router.get('/protected')
+async def protected_route(current_user: Users = Depends(get_current_user)):
+    return current_user
